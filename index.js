5eedb1edbff02aaed2a133d7a987f6f49f6b2113
eee4 <41441600+eee4@users.noreply.github.com>
2025-11-25 11:22:56 +0100

    Com4_1.3

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const express = require('express');
 const app = express();
 
+function isNumeric(obj) {
+  // https://stackoverflow.com/a/175787
+  if (typeof obj != "string") return false;
+  return !isNaN(obj) && !isNaN(parseFloat(obj));
+}
+
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
 //TODO1  
@@ -27,7 +33,24 @@ app.get('/math/rectangle/:width/:height', (req, res) => {
 
 
 //TODO3
+app.get('/math/power/:base/:exponent', (req, res) => { 
+  const base     = req.params.base;
+  const exponent = req.params.exponent;
+  let   root     = req.query.root ?? "false";
 
+  // check if is numeric
+  if (!isNumeric(base) || !isNumeric(exponent)) {
+    res.json({
+      "error": "Invalid input"
+    });
+  }
+
+  const result = {
+    "result": `${Math.pow(base, exponent)}`
+  };
+  if (root === "true") result["root"] = `${Math.sqrt(base)}`;
+  res.json(result);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
