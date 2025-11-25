034955f3519e8b7f489fb2e486a0d8f4076d6881
eee4 <41441600+eee4@users.noreply.github.com>
2025-11-25 11:02:05 +0100

    Com4_1.1

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
 //TODO1  
+  const radius = req.params.r;
+  const result = {
+    "area": `${Math.PI * Math.pow(radius, 2)}`,
+    "circumference": `${radius * 2 * Math.PI}`
+  };
   res.json(result);
 });
 
